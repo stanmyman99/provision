4b2de578a999d032464c667051f2157488b82f3e
stan <stan@myman99>
2023-06-03 20:56:59 -0600

    snap keepass

--- ubuntu-arm64.sh
+++ ubuntu-arm64.sh
@@ -25,7 +25,8 @@ sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub
 
 # Install the common apps using the best app framework the app supports
 #
-flatpak install flathub org.keepassxc.KeePassXC -y
+# flatpak install flathub org.keepassxc.KeePassXC -y
+sudo snap istall keepassxc -y
 flatpak install flathub org.freefilesync.FreeFileSync -y
 flatpak install flathub org.onlyoffice.desktopeditors -y
 flatpak install flathub org.electrum.electrum -y
@@ -57,7 +58,7 @@ popd
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-position LEFT
 gsettings set org.gnome.shell favorite-apps []
 gsettings set org.gnome.shell favorite-apps "['org.mozilla.firefox', 'org.gnome.Terminal.desktop', 'mullvadvpn.desktop', 'org.keepassxc.KeePassXC', 'org.standardnotes.standardnotes.desktop', 'org.onlyoffice.desktopeditors.desktop', 'org.electrum.electrum.desktop', 'org.freefilesync.FreeFileSync.desktop', 'org.gnome.gedit.desktop', 'org.gnome.Calculator.desktop', 'bleachbit-root.desktop', 'gnome-control-center.desktop']"
-gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 20
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 32
 gsettings set org.gnome.desktop.privacy remember-recent-files false
 gsettings set org.gnome.desktop.privacy remove-old-temp-files true
 gsettings set org.gnome.desktop.privacy remove-old-trash-files true
